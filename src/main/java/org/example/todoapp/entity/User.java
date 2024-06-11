3582b4d691485955f617becffa5efae65b53b648
LIM HEE JIN <xeexin0331@gmail.com>
2024-06-11 12:59:51 +0900

    init

---
+++ src/main/java/org/example/todoapp/entity/User.java
@@ -0,0 +1,17 @@
+package org.example.todoapp.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Setter
+@Getter
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+    private String name;
+    private String email;
+}
